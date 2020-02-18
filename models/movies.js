4da565674d8ca4df5d97584eff0e50874d76d38b
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-02-18 10:17:47 -0500

    added more css

--- models/movies.js
+++ models/movies.js
@@ -1,16 +1,21 @@
 const fetch = require('node-fetch')
-const url = "https://api.themoviedb.org/3/movie/now_playing?api_key="
-const api = "8ddbe6143af6afcff9351038999b3629"
+const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=8ddbe6143af6afcff9351038999b3629&language=en-US&page=1'
+
+// const url='https://image/tmdb.org/t/p/w500/8ddbe6143af6afcff9351038999b3629'
 const displayMovie = [];
 const movies = ()=>{
-    return fetch(url+api)
+    return fetch(url)
     .then(res=>res.json())
+    
     .then(({results})=>{ return results.forEach((obj)=> {
-        newArr.push([obj.overview, obj.poster_path]);
+        displayMovie.push([obj.overview, obj.poster_path]);
     }
-    )})
+    
+    )}
+    )
+    
     .catch(err=>console.log('error occurred', err))
 }
 movies();
-
+console.log(displayMovie)
 module.exports = displayMovie;
